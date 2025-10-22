33d5be0b80f137e63eb8f6456c0fab7e13ab42d8
Luk446Ubuntu <luke.edgecomb2@gmail.com>
2025-10-22 11:29:38 +0100

    rm build

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/luke/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/luke/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/luke/catkin_ws/build'
-export PYTHONPATH="/home/luke/catkin_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES='/home/luke/catkin_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/luke/catkin_ws/src:$ROS_PACKAGE_PATH"
