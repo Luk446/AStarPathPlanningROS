da9f346ad5d0b183dc5c5a4dcd00475b0147d0fa
Luk446Ubuntu <luke.edgecomb2@gmail.com>
2025-10-22 12:26:54 +0100

    chore(repo): stop tracking build/ and devel/ (ignored); remove from index

--- build/B31YS_path_planning_assignment_ROS1/src/rosbot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luke/catkin_ws/src/B31YS_path_planning_assignment_ROS1/src/rosbot_bringup
-# Build directory: /home/luke/catkin_ws/build/B31YS_path_planning_assignment_ROS1/src/rosbot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
